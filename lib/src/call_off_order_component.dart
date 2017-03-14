9090e5c1f92abcba7fd6309405fd49b8a24e7c35
Dmitriy.Litovchenko <Dmitriy.Litovchenko@yamalspg.ru>
2017-03-14 18:07:44 +0300

    initial commit

---
+++ lib/src/call_off_order_component.dart
@@ -0,0 +1,45 @@
+import 'dart:html';
+import 'dart:math' as math;
+
+import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+
+import 'package:daterangepicker/daterangepicker.dart';
+import 'package:daterangepicker/daterangepicker_directive.dart';
+
+@Component(
+  selector: 'call-off-order',
+  templateUrl: 'call_off_order_component.html',
+  directives: const [DateRangePickerDirective])
+class CallOffOrderComponent {
+  static const String route_name = 'CallOffOrder';
+  static const String route_path = 'call-off-order';
+  static const Route route = const Route(
+    path: CallOffOrderComponent.route_path,
+    component: CallOffOrderComponent,
+    name: CallOffOrderComponent.route_name,
+    useAsDefault: true);
+
+  final Router _router;
+  DateRangePickerOptions dateRangePickerOptions = new DateRangePickerOptions();
+
+  CallOffOrderComponent(this._router) {
+    var locale = new DateRangePickerLocale()
+      ..format = 'DD.MM.YYYY'
+      ..separator = ' - '
+      ..applyLabel = 'Применить'
+      ..cancelLabel = 'Отменить'
+      ..fromLabel = 'С'
+      ..toLabel = 'По'
+      ..customRangeLabel = 'Custom'
+      ..weekLabel = 'W'
+      ..firstDay = 1
+      ..daysOfWeek = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
+      ..monthNames = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+      'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+
+    dateRangePickerOptions = new DateRangePickerOptions()
+      ..singleDatePicker = true
+      ..locale = locale;
+  }
+}
---
+++ web/main.dart
@@ -0,0 +1,58 @@
+import 'dart:core';
+
+import 'package:angular2/platform/browser.dart';
+import 'package:angular2/core.dart';
+import 'package:angular2/src/core/reflection/reflection.dart';
+import 'package:angular2/router.dart';
+import 'package:angular2/src/platform/browser/location/hash_location_strategy.dart';
+import 'package:angular2/platform/common.dart';
+
+import 'package:http/http.dart';
+import 'package:http/browser_client.dart';
+
+import 'package:alert/alert_service.dart';
+import 'package:angular_utils/cm_router_link.dart';
+import 'package:resources_loader/resources_loader.dart';
+import 'package:master_layout/master_layout_component.dart';
+import 'package:logger/logger_service.dart';
+import 'package:config/config_service.dart';
+import 'package:call_off_order/call_off_order_component.dart';
+
+bool get isDebug =>
+    (const String.fromEnvironment('PRODUCTION', defaultValue: 'false')) !=
+    'true';
+
+@Component(selector: 'app', providers: const [
+  ROUTER_PROVIDERS,
+  const Provider(LocationStrategy, useClass: HashLocationStrategy)
+])
+@View(
+    template: '<master-layout><call-off-order></call-off-order></master-layout>',
+    directives: const [
+      MasterLayoutComponent,
+      CallOffOrderComponent])
+class AppComponent {}
+
+main() async {
+  if (isDebug) {
+    reflector.trackUsage();
+  }
+
+  ComponentRef ref = await bootstrap(AppComponent, [
+    ROUTER_PROVIDERS,
+    const Provider(LocationStrategy, useClass: HashLocationStrategy),
+    const Provider(AlertService),
+    const Provider(LoggerService),
+    const Provider(ConfigService),
+    //provide(Client, useClass: InMemoryDataService)
+    // Using a real back end?
+    // Import browser_client.dart and change the above to:
+    provide(Client, useFactory: () => new BrowserClient(), deps: [])
+  ]);
+
+  if (isDebug) {
+    print('Application in DebugMode');
+    enableDebugTools(ref);
+    print('Unused keys: ${reflector.listUnusedKeys()}');
+  }
+}
